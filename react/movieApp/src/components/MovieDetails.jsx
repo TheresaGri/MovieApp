8d635820d010dafee96e26deba3db6869a340f4e
Viktoria Bzdyk <viktoria.bzdyk@gmx.at>
2023-03-23 20:16:07 +0100

    Add new comment component

--- react/movieApp/src/components/MovieDetails.jsx
+++ react/movieApp/src/components/MovieDetails.jsx
@@ -1,22 +1,30 @@
-import "./MovieDetails.css"
+import './MovieDetails.css';
+import './AddNewComment.jsx';
+import AddNewComment from './AddNewComment.jsx';
 
 export default function MovieDetails(props) {
-  let movie = props.movie;
+	let movie = props.movie;
 
-  return (
-    <div className={`movie-details ${props.className}`}>
-      <h2>{movie.name}</h2>
-      <p>{movie.plot}</p>
-      <div>
-        {" "}
-        Genres:
-        {movie.genres.map((genre) => (
-          <p>{genre}</p>
-        ))}
-      </div>
-      <div> Year: {movie.year}</div>
-      <div> imdb Rating: {movie.imdbRating}</div>
-      <div>duration: {movie.duration} min</div>
-    </div>
-  );
+	const handleNewComment = (e) => {
+		console.log('********');
+		console.log(e);
+	};
+
+	return (
+		<div className={`movie-details ${props.className}`}>
+			<h2>{movie.name}</h2>
+			<p>{movie.plot}</p>
+			<div>
+				{' '}
+				Genres:
+				{movie.genres.map((genre) => (
+					<p>{genre}</p>
+				))}
+			</div>
+			<div> Year: {movie.year}</div>
+			<div> imdb Rating: {movie.imdbRating}</div>
+			<div>duration: {movie.duration} min</div>
+			<AddNewComment onSubmit={handleNewComment}></AddNewComment>
+		</div>
+	);
 }
