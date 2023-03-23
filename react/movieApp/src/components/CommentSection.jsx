3cde315a1a844a67594c2e1cb23dcb2070599c13
Theresa Grimus <T.Grimus@gmx.net>
2023-03-23 20:15:56 +0100

    added some styling to comment section

--- react/movieApp/src/components/CommentSection.jsx
+++ react/movieApp/src/components/CommentSection.jsx
@@ -1,15 +1,18 @@
+import "./CommentSection.css";
 
 export default function CommentSection(props) {
   let comments = props.commentsArray;
 
-  return (<div>
-    {comments.map((comment) => (
-      <div key = {comment._id}>
-        <p>{comment.name}</p>
-        <p>{comment.email}</p>
-        <p>{comment.text}</p>
-        <p>{comment.date}</p>
-      </div>
-    ))}
-  </div>);
+  return (
+    <div className = "comments_container">
+      {comments.map((comment) => (
+        <div key={comment._id} className="comments">
+          <h1>{comment.name}</h1>
+          <p className = "dateOfComment">{comment.date}</p>
+          <p className = "mailOfComment">{comment.email}</p>
+          <p className="textOfComment">{comment.text}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
